59fcd4f70747bd076d640d560c002d5fc8e123e3
Eliah Kagan <degeneracypressure@gmail.com>
2023-01-15 23:22:42 -0500

    simplify, get running

--- src/main/java/io/github/eliahkagan/bedj/Main.java
+++ src/main/java/io/github/eliahkagan/bedj/Main.java
@@ -14,30 +14,24 @@ import com.theokanning.openai.embedding.EmbeddingRequest;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        var records = getWithEmbeddings(List.of(
+        var embeddings = getEmbeddings(List.of(
             "French kissing experts",
             "kissing French experts",
+            "experts in French kissing",
+            "kissing French people who are experts",
             "The canine shall never perish from the earth.",
-            "Somewhere in the world, there will always be dogs."
+            "Somewhere in the world, there will always be dogs.",
+            "Quelque part au monde, il y aura toujours des chiens.",
+            "The tabular data have been sequestered from the public."
         ));
 
-        for (var i = 0; i < records.size(); ++i) {
-            for (var j = 0; j < records.size(); ++j) {
-                
+        for (var i = 0; i < embeddings.size(); ++i) {
+            for (var j = 0; j < embeddings.size(); ++j) {
+                var similarity = dot(embeddings.get(i), embeddings.get(j));
+                System.out.format(" %10f", similarity);
             }
+            System.out.println();
         }
-    }
-
-    private static List<WithEmbed>
-    getWithEmbeddings(List<String> texts) throws IOException {
-        var embeddings = getEmbeddings(texts);
-
-        var zip = StreamUtils.zip(
-            texts.stream(),
-            embeddings.stream(),
-            WithEmbed::new);
-
-        return zip.toList();
     }
 
     private static List<List<Double>>
